07ae5596ff3eff8bdb0740209835c1353366b9b2
leoafarias <leoafarias@gmail.com>
2021-11-17 22:52:56 -0500

    wip

--- lib/src/attributes/common/attribute.dart
+++ lib/src/attributes/common/attribute.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mix/src/attributes/dynamic/dark_mode.attributes.dart';
-import 'package:mix/src/attributes/dynamic/dynamic.utils.dart';
-import 'package:mix/src/attributes/dynamic/media_query.attributes.dart';
 
 /// Base attribute
 
@@ -12,28 +9,16 @@ abstract class Attribute {
   const Attribute();
 }
 
-extension AttributeExtensions on Attribute {
-  DarkModeAttribute get onDark => DarkModeAttribute([this]);
-  // MediaQueryAttribute get onMediaQuery => MediaQueryAttribute(this);
-  ScreenSizeAttribute get onXSmall => DynamicUtils.xsmall([this]);
-  ScreenSizeAttribute get onSmall => DynamicUtils.small([this]);
-  ScreenSizeAttribute get onMedium => DynamicUtils.medium([this]);
-  ScreenSizeAttribute get onLarge => DynamicUtils.large([this]);
-  OrientationAttribute get onPortrait => DynamicUtils.portrait([this]);
-  OrientationAttribute get onLandscape => DynamicUtils.landscape([this]);
-}
-
 /// Allows to pass down Mixes as attributes for use with helpers
 class NestedAttribute<T extends Attribute> extends Attribute {
-  NestedAttribute(this.attributes);
+  const NestedAttribute(this.attributes);
 
   final List<T> attributes;
 }
 
-abstract class DynamicAttribute extends Attribute {
-  const DynamicAttribute(this.attributes);
-
-  final List<Attribute> attributes;
+abstract class DynamicAttribute<T extends Attribute>
+    extends NestedAttribute<T> {
+  const DynamicAttribute(List<T> attributes) : super(attributes);
 
   bool shouldApply(BuildContext context);
 }
