acdaf795b9fa3f5a5bfc8e4f2efa5713a1226b03
leoafarias <leoafarias@gmail.com>
2021-12-10 18:41:44 -0500

    Documentation updates

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -18,46 +18,9 @@ const addClassToDesignTokens = () => {
   }
 }
 
-const addStyleToHighlightLines = () => {
-  // Style design token in code snippets
-  const elements = document.getElementsByClassName('token-line')
-  const elementsArray = Array.from(elements)
-
-  const highlightLines = []
-  if (elementsArray.length > 0) {
-    elementsArray.forEach((el) => {
-      if (
-        el.getAttribute('style') !== null &&
-        el.getAttribute('style') !== ''
-      ) {
-        el.classList.add('highlight')
-        el.parentElement.classList.add('has-highlight')
-        highlightLines.push(el)
-      }
-    })
-    // Add first class for each element that previous simbling does not have class 'highlight'
-    // and last class for each element that next simbling does not have class 'highlight'
-    highlightLines.forEach((el) => {
-      console.log(el)
-      if (
-        el.previousSibling &&
-        !el.previousSibling.classList.contains('highlight')
-      ) {
-        console.log(el.previousSibling)
-        el.classList.add('first')
-      }
-      if (el.nextSibling && !el.nextSibling.classList.contains('highlight')) {
-        console.log(el.nextSibling)
-        el.classList.add('last')
-      }
-    })
-  }
-}
-
 export default function Nextra({ Component, pageProps }) {
   React.useEffect(() => {
     addClassToDesignTokens()
-    addStyleToHighlightLines()
   }, [])
 
   return <Component {...pageProps} />
