fa4d8b7b18e0966614aca5e42f49b440c48621ca
leoafarias <leoafarias@gmail.com>
2021-12-23 12:23:18 -0500

    wip

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,27 +1,28 @@
-import 'nextra-theme-docs/style.css'
-import Prism from 'prism-react-renderer/prism'
-import React from 'react'
-import '../styles.css'
-;(typeof global !== 'undefined' ? global : window).Prism = Prism
-require('prismjs/components/prism-dart')
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+import "nextra-theme-docs/style.css";
+import Prism from "prism-react-renderer/prism";
+import React from "react";
+import "../styles.css";
+
+require("prismjs/components/prism-dart");
 
 const addClassToDesignTokens = () => {
   // Style design token in code snippets
-  const elements = document.getElementsByClassName('token plain')
-  const elementsArray = Array.from(elements)
+  const elements = document.getElementsByClassName("token plain");
+  const elementsArray = Array.from(elements);
   if (elementsArray.length > 0) {
     elementsArray.forEach((el) => {
-      if (el.textContent && el.textContent.startsWith('$')) {
-        el.classList.add('design-token')
+      if (el.textContent && el.textContent.startsWith("$")) {
+        el.classList.add("design-token");
       }
-    })
+    });
   }
-}
+};
 
 export default function Nextra({ Component, pageProps }) {
   React.useEffect(() => {
-    addClassToDesignTokens()
-  }, [])
+    addClassToDesignTokens();
+  }, []);
 
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,26 +1,27 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
+  mode: "jit",
   purge: [
-    './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './theme.config.js',
-    './styles.css',
+    "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./theme.config.js",
+    "./styles.css",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        sans: [`"Inter"`, 'sans-serif'],
+        sans: [`"Inter"`, "sans-serif"],
         mono: [
-          'Menlo',
-          'Monaco',
-          'Lucida Console',
-          'Liberation Mono',
-          'DejaVu Sans Mono',
-          'Bitstream Vera Sans Mono',
-          'Courier New',
-          'monospace',
+          "Menlo",
+          "Monaco",
+          "Lucida Console",
+          "Liberation Mono",
+          "DejaVu Sans Mono",
+          "Bitstream Vera Sans Mono",
+          "Courier New",
+          "monospace",
         ],
       },
       colors: {
@@ -31,13 +32,7 @@ module.exports = {
         red: colors.red,
         yellow: colors.yellow,
       },
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        betterhover: { raw: '(hover: hover)' },
-      },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
