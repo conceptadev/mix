5af45633fda30241b7e6069fb1a321f936db1040
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2022-01-22 11:53:12 -0300

    Implement AlertDialog

--- demo/lib/views/button_preview.dart
+++ demo/lib/views/button_preview.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mix/mix.dart';
 
 PressableWidgetFn get button => Mix(
-      textStyle($caption),
+      textStyle($body2),
       bgColor($primary),
       paddingHorizontal(20),
       paddingVertical(10),
