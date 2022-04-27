8ae3a3b60d7b2749b18d2b8a2b8af12e53109130
leoafarias <leoafarias@gmail.com>
2022-04-27 17:01:33 -0400

    Update demo

--- demo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ demo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import desktop_window
-import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
