e0f09acc98151d73ddb361b8ab5f350c526f73e3
leoafarias <leoafarias@gmail.com>
2022-05-22 13:32:52 -0400

    Material tokens

--- lib/src/theme/material_theme/material_tokens.dart
+++ lib/src/theme/material_theme/material_tokens.dart
@@ -1,6 +1,7 @@
 import 'package:mix/mix.dart';
 
 class MaterialTokens {
+  const MaterialTokens._();
   static final textTheme = MaterialTextThemeTokens();
   static final colorScheme = MaterialColorSchemeTokens();
 }
