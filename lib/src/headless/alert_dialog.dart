f2b1db90c801fdf950196dad8e31b40efab5f61a
leoafarias <leoafarias@gmail.com>
2022-05-22 13:32:29 -0400

    alert

--- lib/src/headless/alert_dialog.dart
+++ lib/src/headless/alert_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mix/mix.dart';
+import 'package:mix/src/theme/material_theme/material_tokens.dart';
 
 /// _Mix_ corollary to Flutter _AlertDialog_ class
 ///
@@ -42,7 +43,7 @@ class AlertDialogX extends RemixableWidget {
   final List<Widget>? actions;
 
   @override
-  Mix<Attribute> get defaultMix {
+  Mix<Attribute> get baseMix {
     return Mix(
       margin(20),
       elevation(6),
@@ -52,19 +53,20 @@ class AlertDialogX extends RemixableWidget {
       crossAxis(CrossAxisAlignment.start),
       mainAxisSize(MainAxisSize.min),
       maxW(500.0),
-      bgColor($surface),
+      bgColor(MaterialTokens.colorScheme.surface),
       paragraph(
-        textStyle($body1),
+        textStyle(MaterialTokens.textTheme.bodyText1),
       ),
       title(
         titleCase(),
-        textStyle($subtitle1.copyWith(fontSize: 20.0)),
+        textStyle(MaterialTokens.textTheme.subtitle1.copyWith(fontSize: 20.0)),
         font(
           weight: FontWeight.bold,
         ),
       ),
       paragraph(
-        textStyle($body1.copyWith(color: Colors.grey.shade700, fontSize: 16.0)),
+        textStyle(MaterialTokens.textTheme.bodyText1
+            .copyWith(color: Colors.grey.shade700, fontSize: 16.0)),
       ),
     );
   }
