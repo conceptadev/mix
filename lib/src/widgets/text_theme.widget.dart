3523dca5e76d099f04e1705c49d30b1ae8bac0b4
leoafarias <leoafarias@gmail.com>
2022-05-22 12:57:35 -0400

    Material text theme widgets

--- lib/src/widgets/text_theme.widget.dart
+++ lib/src/widgets/text_theme.widget.dart
@@ -1,398 +1,506 @@
 import 'package:flutter/material.dart';
+import 'package:mix/src/helpers/extensions.dart';
 
 import '../attributes/exports.dart';
 import '../mixer/mix_factory.dart';
-import '../theme/material_theme/text_theme_tokens.dart';
-import '../theme/refs/text_style_token.dart';
-import 'mixable.widget.dart';
 import 'text.widget.dart';
 
-class BaseType extends RemixableWidget {
-  const BaseType(
+abstract class TextStyleResolverWidget extends StatelessWidget {
+  const TextStyleResolverWidget(
     this.text, {
-    required this.textStyleToken,
-    Mix? mix,
+    this.mix,
     Key? key,
-  }) : super(mix, key: key);
+  }) : super(key: key);
 
   final String text;
-  final TextStyleToken textStyleToken;
+  final Mix? mix;
 
-  @override
-  get defaultMix {
-    return Mix(
-      textStyle(textStyleToken),
-    );
-  }
+  TextStyle? resolver(BuildContext context);
 
   @override
   Widget build(BuildContext context) {
-    return TextMix(text, mix: mix);
+    final style = Mix(
+      textStyle(resolver(context)),
+      apply(mix),
+    );
+    return TextMix(text, mix: style);
   }
 }
 
-class Headline1 extends BaseType {
+class Headline1 extends TextStyleResolverWidget {
   const Headline1(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $headline1,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.headline1;
+  }
 }
 
-class Headline2 extends BaseType {
+class Headline2 extends TextStyleResolverWidget {
   const Headline2(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $headline2,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.headline2;
+  }
 }
 
-class Headline3 extends BaseType {
+class Headline3 extends TextStyleResolverWidget {
   const Headline3(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $headline3,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.headline3;
+  }
 }
 
-class Headline4 extends BaseType {
+class Headline4 extends TextStyleResolverWidget {
   const Headline4(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $headline4,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.headline4;
+  }
 }
 
-class Headline5 extends BaseType {
+class Headline5 extends TextStyleResolverWidget {
   const Headline5(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $headline5,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.headline5;
+  }
 }
 
-class Headline6 extends BaseType {
+class Headline6 extends TextStyleResolverWidget {
   const Headline6(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $headline6,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.headline6;
+  }
 }
 
-class Subtitle1 extends BaseType {
+class Subtitle1 extends TextStyleResolverWidget {
   const Subtitle1(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $subtitle1,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.subtitle1;
+  }
 }
 
-class Subtitle2 extends BaseType {
+class Subtitle2 extends TextStyleResolverWidget {
   const Subtitle2(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $subtitle2,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.subtitle2;
+  }
 }
 
-class BodyText1 extends BaseType {
+class BodyText1 extends TextStyleResolverWidget {
   const BodyText1(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $body1,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.bodyText1;
+  }
 }
 
-class BodyText2 extends BaseType {
+class BodyText2 extends TextStyleResolverWidget {
   const BodyText2(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $body2,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.bodyText2;
+  }
 }
 
-class Caption extends BaseType {
+class Caption extends TextStyleResolverWidget {
   const Caption(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $caption,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.caption;
+  }
 }
 
-class Button extends BaseType {
+class Button extends TextStyleResolverWidget {
   const Button(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $button,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.button;
+  }
 }
 
-class OverLine extends BaseType {
+class OverLine extends TextStyleResolverWidget {
   const OverLine(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $overline,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.overline;
+  }
 }
 
 // Material 3 Typeset
 
-class DisplaySmall extends BaseType {
+class DisplaySmall extends TextStyleResolverWidget {
   const DisplaySmall(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $displaySmall,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.displaySmall;
+  }
 }
 
-class DisplayMedium extends BaseType {
+class DisplayMedium extends TextStyleResolverWidget {
   const DisplayMedium(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $displayMedium,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.displayMedium;
+  }
 }
 
-class DisplayLarge extends BaseType {
+class DisplayLarge extends TextStyleResolverWidget {
   const DisplayLarge(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $displayLarge,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.displayLarge;
+  }
 }
 
-class HeadlineSmall extends BaseType {
+class HeadlineSmall extends TextStyleResolverWidget {
   const HeadlineSmall(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $headlineSmall,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.headlineSmall;
+  }
 }
 
-class HeadlineMedium extends BaseType {
+class HeadlineMedium extends TextStyleResolverWidget {
   const HeadlineMedium(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $headlineMedium,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.headlineMedium;
+  }
 }
 
-class HeadlineLarge extends BaseType {
+class HeadlineLarge extends TextStyleResolverWidget {
   const HeadlineLarge(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $headlineLarge,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.headlineLarge;
+  }
 }
 
-class TitleSmall extends BaseType {
+class TitleSmall extends TextStyleResolverWidget {
   const TitleSmall(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $titleSmall,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.titleSmall;
+  }
 }
 
-class TitleMedium extends BaseType {
+class TitleMedium extends TextStyleResolverWidget {
   const TitleMedium(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $titleMedium,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.titleMedium;
+  }
 }
 
-class TitleLarge extends BaseType {
+class TitleLarge extends TextStyleResolverWidget {
   const TitleLarge(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $titleLarge,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.titleLarge;
+  }
 }
 
-class BodySmall extends BaseType {
+class BodySmall extends TextStyleResolverWidget {
   const BodySmall(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $bodySmall,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.bodySmall;
+  }
 }
 
-class BodyMedium extends BaseType {
+class BodyMedium extends TextStyleResolverWidget {
   const BodyMedium(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $bodyMedium,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.bodyMedium;
+  }
 }
 
-class BodyLarge extends BaseType {
+class BodyLarge extends TextStyleResolverWidget {
   const BodyLarge(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $bodyLarge,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.bodyLarge;
+  }
 }
 
-class LabelSmall extends BaseType {
+class LabelSmall extends TextStyleResolverWidget {
   const LabelSmall(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $labelSmall,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.labelSmall;
+  }
 }
 
-class LabelMedium extends BaseType {
+class LabelMedium extends TextStyleResolverWidget {
   const LabelMedium(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $labelMedium,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.labelMedium;
+  }
 }
 
-class LabelLarge extends BaseType {
+class LabelLarge extends TextStyleResolverWidget {
   const LabelLarge(
     String text, {
     Mix? mix,
     Key? key,
   }) : super(
           text,
-          textStyleToken: $labelLarge,
           mix: mix,
           key: key,
         );
+
+  @override
+  TextStyle? resolver(BuildContext context) {
+    return context.theme.textTheme.labelLarge;
+  }
 }
