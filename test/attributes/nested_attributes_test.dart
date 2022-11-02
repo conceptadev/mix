3ca4c2d8a82686b7cf07c7db971f3bc725a1244e
leoafarias <leoafarias@gmail.com>
2022-11-02 10:18:33 -0400

    Implemented tests for nested mix

---
+++ test/attributes/nested_attributes_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mix/mix.dart';
+import 'package:mix/src/helpers/dto/edge_insets.dto.dart';
+
+void main() {
+  group("Nested Attributes", () {
+    test('-> Apply Mixes', () async {
+      final styleMargin = Mix(
+        const BoxAttributes(
+          margin: EdgeInsetsDto.all(20),
+        ),
+      );
+      final styleColor = Mix(
+        const BoxAttributes(color: Colors.red),
+      );
+      final nestedStyle = Mix(
+        apply(styleColor, styleMargin),
+      );
+
+      final boxAttributes =
+          nestedStyle.values.attributesOfType<BoxAttributes>();
+
+      expect(
+        boxAttributes,
+        const BoxAttributes(
+          color: Colors.red,
+          margin: EdgeInsetsDto.all(20),
+        ),
+      );
+    });
+  });
+}
