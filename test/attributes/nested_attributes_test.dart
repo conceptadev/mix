360004bd68f26f67ece06a3756608299c6ec0a6e
leoafarias <leoafarias@gmail.com>
2022-11-02 10:23:05 -0400

    Further tests

--- test/attributes/nested_attributes_test.dart
+++ test/attributes/nested_attributes_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mix/mix.dart';
+import 'package:mix/src/attributes/nested_attribute.dart';
 import 'package:mix/src/helpers/dto/edge_insets.dto.dart';
 
 void main() {
@@ -15,14 +16,34 @@ void main() {
         const BoxAttributes(color: Colors.red),
       );
       final nestedStyle = Mix(
+        NestedMixAttribute(
+          Mix.combineAll([
+            styleMargin,
+            styleColor,
+          ]),
+        ),
+      );
+
+      final nestedStyleUtility = Mix(
         apply(styleColor, styleMargin),
       );
 
       final boxAttributes =
           nestedStyle.values.attributesOfType<BoxAttributes>();
 
+      final boxAttributesUtility =
+          nestedStyleUtility.values.attributesOfType<BoxAttributes>();
+
       expect(
         boxAttributes,
+        const BoxAttributes(
+          color: Colors.red,
+          margin: EdgeInsetsDto.all(20),
+        ),
+      );
+
+      expect(
+        boxAttributesUtility,
         const BoxAttributes(
           color: Colors.red,
           margin: EdgeInsetsDto.all(20),
