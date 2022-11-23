555969183627b940abf6dbbde76e1db55dcb0ca8
Bruno D'Luka <brunodlukaa@gmail.com>
2022-11-23 16:31:21 -0300

    Add common requirements and dependencies

--- benchmarks/bin/benchmarks.dart
+++
@@ -1,5 +0,0 @@
-import 'package:benchmarks/benchmarks.dart' as benchmarks;
-
-void main(List<String> arguments) {
-  print('Hello world: ${benchmarks.calculate()}!');
-}
--- benchmarks/test/benchmarks_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:benchmarks/benchmarks.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
