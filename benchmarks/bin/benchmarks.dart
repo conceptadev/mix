07c365725fdd724db6967e5ece0af2ce9552de84
Bruno D'Luka <brunodlukaa@gmail.com>
2022-11-23 16:26:57 -0300

    Create benchmarks project

---
+++ benchmarks/bin/benchmarks.dart
@@ -0,0 +1,5 @@
+import 'package:benchmarks/benchmarks.dart' as benchmarks;
+
+void main(List<String> arguments) {
+  print('Hello world: ${benchmarks.calculate()}!');
+}
---
+++ benchmarks/test/benchmarks_test.dart
@@ -0,0 +1,8 @@
+import 'package:benchmarks/benchmarks.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
