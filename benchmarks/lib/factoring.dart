abbd19ed89d0c66feb1199e6545abcb3afb246b2
Bruno D'Luka <brunodlukaa@gmail.com>
2022-11-30 10:31:37 -0300

    Increase bench factoring coverage

--- benchmarks/lib/factoring.dart
+++ benchmarks/lib/factoring.dart
@@ -2,6 +2,7 @@ import 'package:benchmarks/common.dart';
 import 'package:flutter/material.dart';
 import 'package:mix/mix.dart';
 
+/// flutter run -d windows -t lib/factoring.dart --release
 void main() {
   assert(
     false,
@@ -11,49 +12,106 @@ void main() {
   final printer = BenchmarkResultPrinter();
   final watch = Stopwatch();
 
-  final firstMix = Mix(
-    height(100),
-    animated(),
-    marginY(10),
-    // elevation(10),
-    rounded(10),
-    bgColor(Color(0xFF00FF80)),
-    textStyle(TextStyle()),
-    textColor(Color(0xFFFFFFFF)),
-    onHover(
-      // elevation(2),
-      padding(20),
-      bgColor(Color(0xFFFFFF80)),
-      textColor(Color(0xFF000000)),
-    ),
-  );
+  {
+    final firstMix = Mix(
+      height(100),
+      animated(),
+      marginY(10),
+      rounded(10),
+      bgColor(Color(0xFF00FF80)),
+      textStyle(TextStyle()),
+      textColor(Color(0xFFFFFFFF)),
+      onHover(
+        padding(20),
+        bgColor(Color(0xFFFFFF80)),
+        textColor(Color(0xFF000000)),
+      ),
+    );
 
-  final secondMix = Mix(
-    width(100),
-    marginX(10),
-    shadow(spreadRadius: 100),
-    rounded(100),
-    bgColor(Color(0xFF00FF80)),
-    borderColor(Color(0xFF984F9B)),
-    onHover(
-      shadow(spreadRadius: 2),
-      margin(20),
-      bgColor(Color(0xFFFFFF80)),
-      textColor(Color(0xFF000000)),
-    ),
-  );
+    final secondMix = Mix(
+      width(100),
+      marginX(10),
+      shadow(spreadRadius: 100),
+      rounded(100),
+      bgColor(Color(0xFF00FF80)),
+      borderColor(Color(0xFF984F9B)),
+      onPress(
+        shadow(spreadRadius: 2),
+        margin(20),
+        bgColor(Color(0xFFFFFF80)),
+        textColor(Color(0xFF000000)),
+      ),
+    );
 
-  watch.start();
+    watch.start();
 
-  Mix.combine(firstMix, secondMix);
+    Mix.combine(firstMix, secondMix);
 
-  watch.stop();
+    watch.stop();
 
-  printer.addResult(
-    description: 'Combine two extense mixes',
-    value: watch.elapsedMicroseconds.toDouble(),
-    unit: 'microseconds',
-    name: 'combine_mix_bench',
-  );
+    printer.addResult(
+      description: 'Combine two extense mixes',
+      value: watch.elapsedMicroseconds.toDouble(),
+      unit: 'microseconds',
+      name: 'combine_mix_bench',
+    );
+  }
+
+  watch.reset();
+
+  {
+    final hasError = Variant('hasError');
+    final mix = Mix(
+      hasError(
+        padding(20),
+        bgColor(Color(0xFFFFFF80)),
+        textColor(Color(0xFF000000)),
+      ),
+    );
+
+    watch.start();
+
+    mix.withVariant(hasError);
+
+    watch.stop();
+
+    printer.addResult(
+      description: 'Apply a variant to a mix',
+      value: watch.elapsedMicroseconds.toDouble(),
+      unit: 'microseconds',
+      name: 'apply_variant_bench',
+    );
+  }
+
+  {
+    final mix = Mix(
+      onDisabled(padding(20)),
+      onFocus(bgColor(Color(0xFFFFFF80))),
+      onHover(textColor(Color(0xFF000000))),
+      onPress(textBgColor(Color(0xFF964234))),
+      onDark(textHeight(10)),
+      onLight(textShadow(color: Color(0xFF000000))),
+    );
+
+    watch.start();
+
+    mix.withManyVariants([
+      onDisabled,
+      onFocus,
+      onHover,
+      onPress,
+      onDark,
+      onLight,
+    ]);
+
+    watch.stop();
+
+    printer.addResult(
+      description: 'Apply multiple variants to a mix',
+      value: watch.elapsedMicroseconds.toDouble(),
+      unit: 'microseconds',
+      name: 'apply_multiple_variants_bench',
+    );
+  }
   printer.printToStdout();
 }
--- benchmarks/lib/main.dart
+++ benchmarks/lib/main.dart
@@ -1,5 +1,7 @@
+import 'context.dart' as context_bench;
 import 'factoring.dart' as factoring_bench;
 
 void main() {
   factoring_bench.main();
+  context_bench.main();
 }
