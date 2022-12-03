4cf9dadcf4f75040da73521b6ce54fffac273ec7
Bruno D'Luka <brunodlukaa@gmail.com>
2022-12-03 10:54:55 -0300

    Update main.dart

--- benchmarks/lib/main.dart
+++ benchmarks/lib/main.dart
@@ -1,7 +1,9 @@
 import 'context.dart' as context_bench;
 import 'factoring.dart' as factoring_bench;
+import 'values.dart' as values_bench;
 
 void main() {
-  factoring_bench.main();
   context_bench.main();
+  factoring_bench.main();
+  values_bench.main();
 }
