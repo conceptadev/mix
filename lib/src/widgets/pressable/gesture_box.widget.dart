0fa1d22194f063bb910375ddbcde7f90a8763ed2
leoafarias <leoafarias@gmail.com>
2022-12-22 15:43:20 -0500

    Updated Pressable and GestureBox

--- lib/src/widgets/pressable/gesture_box.widget.dart
+++ lib/src/widgets/pressable/gesture_box.widget.dart
@@ -7,7 +7,7 @@ class GestureBox extends StatelessWidget {
     required this.child,
     this.onTap,
     this.mix = Mix.constant,
-    this.onLongPressed,
+    this.onLongPress,
     this.focusNode,
     this.autofocus = false,
     this.behavior,
@@ -16,7 +16,7 @@ class GestureBox extends StatelessWidget {
 
   final MixableWidget child;
   final VoidCallback? onTap;
-  final VoidCallback? onLongPressed;
+  final VoidCallback? onLongPress;
   final FocusNode? focusNode;
   final bool autofocus;
   final HitTestBehavior? behavior;
@@ -27,7 +27,7 @@ class GestureBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureBoxMixerWidget(
       onTap: onTap,
-      onLongPress: onLongPressed,
+      onLongPress: onLongPress,
       focusNode: focusNode,
       autofocus: autofocus,
       mix: mix,
@@ -90,7 +90,7 @@ class GestureBoxMixerWidgetState extends State<GestureBoxMixerWidget> {
   }
 
   bool _onHover = false;
-  bool _onShouldShowFocus = false;
+  bool _onFocus = false;
   bool _onPress = false;
   bool _onLongPress = false;
 
@@ -98,7 +98,7 @@ class GestureBoxMixerWidgetState extends State<GestureBoxMixerWidget> {
 
   void unpress() {
     Future.delayed(const Duration(milliseconds: 100), () {
-      if (!_onLongPress) {
+      if (_onPress) {
         updateState(() => _onPress = false);
       }
     });
@@ -116,42 +116,41 @@ class GestureBoxMixerWidgetState extends State<GestureBoxMixerWidget> {
         enabled: enabled,
         focusable: enabled && node.canRequestFocus,
         focused: node.hasFocus,
-        child: FocusableActionDetector(
-          focusNode: node,
-          autofocus: widget.autofocus,
-          enabled: enabled,
-          onShowFocusHighlight: (v) {
-            updateState(() => _onShouldShowFocus = v);
+        child: GestureDetector(
+          behavior: widget.behavior,
+          onTap: () {
+            widget.onTap?.call();
           },
-          onShowHoverHighlight: (v) {
-            updateState(() => _onHover = v);
+          onTapDown: (_) {
+            updateState(() => _onPress = true);
           },
-          child: GestureDetector(
-            behavior: widget.behavior,
-            onTap: () {
-              widget.onTap?.call();
+          onTapUp: (_) {
+            unpress();
+          },
+          onTapCancel: () {
+            unpress();
+          },
+          onLongPress: widget.onLongPress,
+          onLongPressStart: (_) {
+            updateState(() => _onLongPress = true);
+          },
+          onLongPressEnd: (_) {
+            updateState(() => _onLongPress = false);
+          },
+          child: FocusableActionDetector(
+            focusNode: node,
+            autofocus: widget.autofocus,
+            enabled: enabled,
+            onShowFocusHighlight: (v) {
+              updateState(() => _onFocus = v);
             },
-            onTapDown: (_) {
-              updateState(() => _onPress = true);
-            },
-            onTapUp: (_) {
-              unpress();
-            },
-            onTapCancel: () {
-              unpress();
-            },
-            onLongPressStart: (_) {
-              updateState(() => _onPress = true);
-              updateState(() => _onLongPress = true);
-            },
-            onLongPressEnd: (_) {
-              updateState(() => _onLongPress = false);
-              updateState(() => _onPress = false);
+            onShowHoverHighlight: (v) {
+              updateState(() => _onHover = v);
             },
             child: Box(
               mix: widget.mix.withManyVariants([
                 if (!enabled) onDisabled,
-                if (_onShouldShowFocus) onFocus,
+                if (_onFocus) onFocus,
                 if (_onHover) onHover,
                 if (_onPress) onPress,
                 if (_onLongPress) onLongPress,
