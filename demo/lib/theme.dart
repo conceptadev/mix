24e6aabf664c502b001ee96f5ed6ec10238bf589
Eduardo M <contact@eduardom.dev>
2023-06-28 17:11:18 +0200

    Switch to M3 for demo

--- demo/lib/theme.dart
+++ demo/lib/theme.dart
@@ -24,9 +24,20 @@ ThemeData _customThemeBuilder(ThemeData theme) {
 ThemeData get darkTheme {
   return _customThemeBuilder(
     ThemeData.dark().copyWith(
+      useMaterial3: true,
       colorScheme: const ColorScheme.dark().copyWith(
         primary: Colors.purpleAccent,
         secondary: Colors.purpleAccent,
+        primaryContainer: Colors.purpleAccent,
+        secondaryContainer: Colors.purpleAccent.shade100,
+      ),
+      switchTheme: SwitchThemeData(
+        thumbColor: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.hovered)) {
+            return Colors.white;
+          }
+          return Colors.white.withAlpha(230);
+        }),
       ),
     ),
   );
@@ -36,9 +47,12 @@ ThemeData get lightTheme {
   return _customThemeBuilder(
     ThemeData.light().copyWith(
       scaffoldBackgroundColor: Colors.white,
+      useMaterial3: true,
       colorScheme: const ColorScheme.light().copyWith(
         primary: Colors.deepPurpleAccent,
         secondary: Colors.deepPurple,
+        primaryContainer: Colors.deepPurpleAccent,
+        secondaryContainer: Colors.deepPurpleAccent.shade100,
       ),
     ),
   );
