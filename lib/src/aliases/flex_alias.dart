1e00dca44182c2516cdde57d28ef6a997c5bafea
Eduardo M <contact@eduardom.dev>
2023-07-10 18:57:20 +0200

    Fix deprecated code

--- lib/src/aliases/flex_alias.dart
+++ lib/src/aliases/flex_alias.dart
@@ -4,5 +4,5 @@ final flexDirection = const FlexUtilities().direction;
 final verticalDirection = const FlexUtilities().verticalDirection;
 final mainAxisAlignment = const FlexUtilities().mainAxisAlignment;
 final mainAxisSize = const FlexUtilities().mainAxisSize;
-final crossAxis = const FlexUtilities().crossAxis;
+final crossAxis = const FlexUtilities().crossAxisAlignment;
 final gap = const FlexUtilities().gap;
