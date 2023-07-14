d372451798e7efc58ca8c41f86eab7a09488aea5
Eduardo M <contact@eduardom.dev>
2023-07-14 17:19:33 +0200

    WIP: add tests for variants

--- test/variants/variants_test.dart
+++ test/variants/variants_test.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mix/src/aliases/container_alias.dart';
-import 'package:mix/src/aliases/context_variants_alias.dart';
-import 'package:mix/src/extensions/build_context_ext.dart';
-import 'package:mix/src/factory/style_mix.dart';
-import 'package:mix/src/widgets/container/container.widget.dart';
+import 'package:mix/mix.dart';
 
 void main() {
   testWidgets('System Theme Variants', (tester) async {
@@ -53,4 +49,123 @@ void main() {
     final colorWidget = tester.widget<Container>(find.byType(Container));
     expect(colorWidget.color, Colors.black);
   });
+
+  testWidgets('Variant onDark light', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      home: StyledContainer(
+        style: StyleMix(
+          onDark(backgroundColor(Colors.teal)),
+          backgroundColor(Colors.red),
+        ),
+      ),
+    ));
+
+    final colorWidget = tester.widget<Container>(find.byType(Container));
+    expect(colorWidget.color, isNot(equals(Colors.black)));
+  });
+
+  testWidgets('Variant onDark dark', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: StyledContainer(
+        style: StyleMix(
+          onDark(backgroundColor(Colors.teal)),
+          backgroundColor(Colors.red),
+        ),
+      ),
+    ));
+
+    final colorWidget = tester.widget<Container>(find.byType(Container));
+    expect(colorWidget.color, Colors.teal);
+  });
+
+  testWidgets('Variant onHover', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: Pressable(
+        onPressed: () {},
+        child: StyledContainer(
+          style: StyleMix(
+            onHover(backgroundColor(Colors.teal)),
+            backgroundColor(Colors.red),
+          ),
+        ),
+      ),
+    ));
+
+    final colorWidget = tester.widget<Container>(find.byType(Container));
+    expect(colorWidget.color, Colors.red);
+
+    // TODO: Figure out how to simulate hover
+    /*
+    final gesture = await tester.createGesture(kind: PointerDeviceKind.mouse);
+    await gesture.addPointer(location: Offset.zero);
+    addTearDown(() => gesture.removePointer());
+    await tester.pump();
+    await gesture.moveTo(tester.getCenter(find.byType(StyledContainer)));
+    await tester.pumpAndSettle();
+
+    expect(colorWidget.color, Colors.teal);
+    */
+  });
+
+  testWidgets('Variant onXSmall', (tester) async {
+    await tester.pumpWidget(MaterialApp(
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: StyledContainer(
+        style: StyleMix(
+          onXSmall(backgroundColor(Colors.teal)),
+          backgroundColor(Colors.red),
+        ),
+      ),
+    ));
+
+    final colorWidget = tester.widget<Container>(find.byType(Container));
+    expect(colorWidget.color, Colors.red);
+
+    // Set screen size to xsmall
+    await tester.binding.setSurfaceSize(const Size(100, 100));
+    addTearDown(() => tester.binding.setSurfaceSize(null));
+
+    await tester.pumpAndSettle();
+
+    final colorWidget2 = tester.widget<Container>(find.byType(Container));
+
+    expect(colorWidget2.color, Colors.teal);
+  });
 }
+
+/*
+onXSmall
+onMedium
+onSmall
+onLarge
+
+onPortrait
+onLandscape
+
+onDark
+onLight
+
+onRTL
+onLTR
+
+onDisabled
+onEnabled
+
+onFocus
+onHover
+onPress
+onLongPress
+
+onNot
+*/
