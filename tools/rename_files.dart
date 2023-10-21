6573c385d5e217567da87029864c10e2792c76b3
Leo Farias <leoafarias@gmail.com>
2023-10-20 22:24:42 -0400

    Rename file script

--- tools/rename_files.dart
+++ tools/rename_files.dart
@@ -1,58 +1,34 @@
-#!/usr/bin/env dartimport 'dart:io';
-
 import 'dart:io';
 
 import 'package:path/path.dart' as p;
 
 void main() {
-  final dir = Directory('./lib');
+  final dir = Directory('./lib'); // Starting directory
   const searchPattern = '.attributes.dart';
   const replacement = '_attribute.dart';
 
-  // 1. Recursively get all the Dart files
-  final allDartFiles = dir
-      .listSync(recursive: true)
-      .where((entity) => entity is File && entity.path.endsWith('.dart'))
-      .cast<File>();
+  // 1. Update imports in all Dart files
+  final allDartFiles = dir.listSync(recursive: true).where((entity) {
+    return entity is File && entity.path.endsWith('.dart');
+  }).cast<File>();
 
-  // Initialize renamedFiles map
-  final renamedFiles = <String, String>{};
-
-  // 2. Loop over each Dart file to update its import statements
   for (final file in allDartFiles) {
-    var content = file.readAsStringSync();
-    bool hasChanges = false;
+    String content = file.readAsStringSync();
 
-    // Try updating imports in the current file based on previously renamed files
-    for (final oldPath in renamedFiles.keys) {
-      final oldImport = p.basename(oldPath);
-      final newImport = p.basename(renamedFiles[oldPath]!);
-      final oldImportWithoutExtension =
-          oldImport.replaceAll(p.extension(searchPattern), '');
+    content = content.replaceAll(searchPattern, replacement);
 
-      if (content.contains('import \'$oldImportWithoutExtension.dart\'')) {
-        content = content.replaceAll(
-          'import \'$oldImportWithoutExtension.dart\'',
-          'import \'$newImport\'',
-        );
-        hasChanges = true;
-      }
-    }
+    file.writeAsStringSync(content);
+  }
 
-    if (hasChanges) {
-      file.writeAsStringSync(content);
-    }
-
-    // Rename the file if its name contains the search pattern
+  // 2. Rename matching files
+  for (final file in allDartFiles) {
     final filename = p.basename(file.path);
     if (filename.contains(searchPattern)) {
       final newFilename = filename.replaceAll(searchPattern, replacement);
       final newPath = p.join(p.dirname(file.path), newFilename);
-
       file.renameSync(newPath);
-      renamedFiles[file.path] = newPath;
     }
   }
 
-  print('File renaming and import updates completed.');
+  print('Import updates and file renaming completed.');
 }
