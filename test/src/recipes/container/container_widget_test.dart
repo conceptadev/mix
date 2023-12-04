2b584449389f95a50090a10693597401b15a01cd
Leo Farias <leoafarias@gmail.com>
2023-12-04 08:14:22 -0500

    Finished test

--- test/src/recipes/container/container_widget_test.dart
+++ test/src/recipes/container/container_widget_test.dart
@@ -7,7 +7,7 @@ import '../../../helpers/testing_utils.dart';
 void main() {
   testWidgets('StyledContainer', (WidgetTester tester) async {
     final paddingAttr = padding(10);
-    final marginAttr = margin(10);
+    final marginAttr = margin(15);
     final alignmentAttr = alignment.center();
     final clipAttr = clipBehavior.hardEdge();
 
@@ -32,13 +32,17 @@ void main() {
     final containerFinder = find.byType(Container);
     final containerWidget = tester.widget<Container>(containerFinder);
 
-    final containerSpec = boxDecorationAttr.resolve(EmptyMixData);
-
-    expect(containerWidget.padding, containerSpec.padding);
-    expect(containerWidget.margin, containerSpec.margin);
-    expect(containerWidget.alignment, containerSpec.alignment);
-    expect(containerWidget.clipBehavior, containerSpec.clipBehavior);
-    expect(containerWidget.decoration, isA<BoxDecoration>());
-    expect(containerWidget.decoration, equals(containerSpec.decoration));
+    expect(containerWidget.padding, const EdgeInsets.all(10));
+    expect(containerWidget.margin, const EdgeInsets.all(15));
+    expect(containerWidget.alignment, Alignment.center);
+    expect(containerWidget.clipBehavior, Clip.hardEdge);
+    expect(
+        containerWidget.decoration,
+        BoxDecoration(
+          border:
+              Border.all(color: Colors.red, width: 1, style: BorderStyle.solid),
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.red,
+        ));
   });
 }
