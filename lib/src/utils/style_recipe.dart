b116eb76154d10366cd9e238f6327bdc52a776df
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-01-17 19:56:40 -0300

    Review doc

---
+++ lib/src/utils/style_recipe.dart
@@ -0,0 +1,11 @@
+import '../../mix.dart';
+
+abstract class StyleRecipe<T extends StyleRecipe<T>> {
+  const StyleRecipe();
+
+  StyleRecipe<T> merge(covariant StyleRecipe<T>? other);
+
+  StyleRecipe<T> selectVariants(List<Variant> variants);
+
+  StyleRecipe<T> copyWith();
+}
