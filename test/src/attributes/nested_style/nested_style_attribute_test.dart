d3661b469223281573166285212c9e0e2a21cb1a
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-03-15 16:28:31 -0300

    Update nested_style_attribute_test.dart

--- test/src/attributes/nested_style/nested_style_attribute_test.dart
+++ test/src/attributes/nested_style/nested_style_attribute_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mix/mix.dart';
+import 'package:mix/src/attributes/nested_style/nested_style_attribute.dart';
+import 'package:mix/src/factory/style_mix.dart';
 
 import '../../../helpers/testing_utils.dart';
 
