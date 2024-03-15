78cef9cafac638403344ebc1e585ee6fde9207ff
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-03-15 15:28:19 -0300

    remove and rename the pre-built tokens

--- lib/src/theme/tokens/token_util.dart
+++ lib/src/theme/tokens/token_util.dart
@@ -2,42 +2,22 @@ import 'package:flutter/material.dart';
 
 import '../material/material_tokens.dart';
 import 'breakpoints_token.dart';
-import 'radius_token.dart';
-import 'space_token.dart';
 
 const $md = MaterialTokens();
-const $radii = RadiiTokenUtil();
-const $space = SpaceTokenUtil();
+const $radii = RadiusTokenUtil();
+const $spaces = SpaceTokenUtil();
 const $colors = ColorTokenUtil();
 const $breakpoints = BreakpointTokenUtil();
 const $textStyles = TextStyleTokenUtil();
 
 @immutable
-class RadiiTokenUtil {
-  final small = RadiusToken.small;
-  final medium = RadiusToken.medium;
-  final large = RadiusToken.large;
-  const RadiiTokenUtil();
+class RadiusTokenUtil {
+  const RadiusTokenUtil();
 }
 
 @immutable
 class SpaceTokenUtil {
-  final xsmall = SpaceToken.xsmall;
-  final small = SpaceToken.small;
-  final medium = SpaceToken.medium;
-  final large = SpaceToken.large;
-  final xlarge = SpaceToken.xlarge;
-  final xxlarge = SpaceToken.xxlarge;
   const SpaceTokenUtil();
-}
-
-@immutable
-class BreakpointTokenUtil {
-  final xsmall = BreakpointToken.xsmall;
-  final small = BreakpointToken.small;
-  final medium = BreakpointToken.medium;
-  final large = BreakpointToken.large;
-  const BreakpointTokenUtil();
 }
 
 @immutable
@@ -48,4 +28,13 @@ class ColorTokenUtil {
 @immutable
 class TextStyleTokenUtil {
   const TextStyleTokenUtil();
+}
+
+@immutable
+class BreakpointTokenUtil {
+  final xsmall = BreakpointToken.xsmall;
+  final small = BreakpointToken.small;
+  final medium = BreakpointToken.medium;
+  final large = BreakpointToken.large;
+  const BreakpointTokenUtil();
 }
