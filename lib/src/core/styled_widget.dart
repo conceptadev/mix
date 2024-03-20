76a955405d65305efb098f3e30536c58cd5362d4
Leo Farias <leoafarias@gmail.com>
2024-03-20 13:35:31 -0400

    Updated imports

--- lib/src/core/styled_widget.dart
+++ lib/src/core/styled_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-import '../../mix.dart';
+import '../decorators/widget_decorator_widget.dart';
+import '../factory/mix_provider.dart';
+import '../factory/mix_provider_data.dart';
+import '../factory/style_mix.dart';
 
 /// An abstract widget for applying custom styles.
 ///
