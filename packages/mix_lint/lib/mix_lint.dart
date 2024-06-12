2d4489707bc1275b49cf93aa4a4218ae33134f12
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-06-12 18:02:20 -0300

    create a new lint rule avoid_empty_style

--- packages/mix_lint/lib/mix_lint.dart
+++ packages/mix_lint/lib/mix_lint.dart
@@ -1,5 +1,6 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
+import 'src/lints/avoid_empty_style.dart';
 import 'src/lints/avoid_variant_inside_context_variant.dart';
 import 'src/lints/externalize_token_instantiation.dart';
 import 'src/lints/externalize_variant_instantiation.dart';
@@ -10,6 +11,7 @@ PluginBase createPlugin() => _MixLint();
 class _MixLint extends PluginBase {
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
+        AvoidEmptyStyle(),
         AttributesOrderInStyle(),
         ExternalizeVariantInstantiation(),
         ExternalizeTokenInstantiation(),
