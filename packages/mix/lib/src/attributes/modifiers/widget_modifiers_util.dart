673fb1334c1739757e9030c4eb3abb47c4769bf5
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-06-28 10:33:51 -0300

    reusing utilities

--- packages/mix/lib/src/attributes/modifiers/widget_modifiers_util.dart
+++ packages/mix/lib/src/attributes/modifiers/widget_modifiers_util.dart
@@ -1,35 +1,12 @@
 import '../../core/core.dart';
 import '../../modifiers/modifiers.dart';
-import '../spacing/spacing_util.dart';
 import 'widget_modifiers_data_dto.dart';
 
-final class ModifierUtility<T extends Attribute>
-    extends MixUtility<T, WidgetModifiersDataDto> {
-  late final intrinsicWidth = IntrinsicWidthWidgetUtility(only);
-  late final intrinsicHeight = IntrinsicHeightWidgetUtility(only);
-  late final rotate = RotatedBoxWidgetUtility(only);
-  late final opacity = OpacityUtility(only);
-  late final clipPath = ClipPathUtility(only);
-  late final clipRRect = ClipRRectUtility(only);
-  late final clipOval = ClipOvalUtility(only);
-  late final clipRect = ClipRectUtility(only);
-  late final clipTriangle = ClipTriangleUtility(only);
-  late final visibility = VisibilityUtility(only);
-  late final show = visibility.on;
-  late final hide = visibility.off;
-  late final aspectRatio = AspectRatioUtility(only);
-  late final flexible = FlexibleModifierUtility(only);
-  late final expanded = flexible.expanded;
-  late final transform = TransformUtility(only);
+final class SpecModifierUtility<T extends Attribute>
+    extends ModifierUtility<T, WidgetModifiersDataDto> {
+  SpecModifierUtility(super.builder);
 
-  late final scale = transform.scale;
-  late final align = AlignWidgetUtility(only);
-  late final fractionallySizedBox = FractionallySizedBoxModifierUtility(only);
-  late final sizedBox = SizedBoxModifierUtility(only);
-  late final padding = SpacingUtility(PaddingModifierUtility(only));
-
-  ModifierUtility(super.builder);
-
+  @override
   T only(WidgetModifierAttribute attribute) {
     return builder(WidgetModifiersDataDto([attribute]));
   }
