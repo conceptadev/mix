caa3c6883f3118d97f4a70ce7f5871d5b49aebf5
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-07-09 11:31:50 -0300

    Removing unnecessary imports

--- packages/mix/lib/src/attributes/text_style/text_style_util.dart
+++ packages/mix/lib/src/attributes/text_style/text_style_util.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/widgets.dart';
 
 import '../../core/attribute.dart';
--- packages/mix/test/src/attributes/text_style/text_style_dto_test.dart
+++ packages/mix/test/src/attributes/text_style/text_style_dto_test.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mix/mix.dart';
