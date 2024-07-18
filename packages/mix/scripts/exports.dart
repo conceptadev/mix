de9a079e500f14a82975140e678c38a51cb24c0c
Leo Farias <leoafarias@gmail.com>
2024-07-18 10:14:20 -0400

    Improved export script

--- packages/mix/scripts/exports.dart
+++ packages/mix/scripts/exports.dart
@@ -29,18 +29,17 @@ Future<void> _libraryExport() async {
   // Traverse the /lib/ directory
   for (final key in fileMap.keys) {
     final importList = fileMap[key]!;
-    final barrelDir = Directory(_joinPaths(_srcDirectory.path, key));
-    final barreFile = File(_joinPaths(barrelDir.path, '$key.dart'));
+    // final barrelDir = Directory(_joinPaths(_srcDirectory.path, key));
+    // final barreFile = File(_joinPaths(barrelDir.path, '$key.dart'));
+    // barreFile.deleteSync();
+    libOutputString.writeln('/// ${key.toUpperCase()}');
+
     final barrelOutput = StringBuffer();
     barrelOutput.write(_generatedMessage);
     for (final import in importList) {
-      final relativePath = import.replaceAll('src/$key/', '');
-      barrelOutput.writeln('export \'$relativePath\';');
+      final relativePath = import.replaceAll('src/', '');
+      libOutputString.writeln('export \'$relativePath\';');
     }
-
-    barreFile.writeAsStringSync(barrelOutput.toString());
-    final relativePath = _getRelativePath(barreFile.path, _libDirectory.path);
-    libOutputString.writeln('export \'$relativePath\';');
   }
 
   _mixFile.writeAsStringSync(libOutputString.toString());
