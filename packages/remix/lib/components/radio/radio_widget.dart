18227a189ebdbec1164d6e2a3f0ca5c1b386d6be
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-08-08 13:02:27 -0300

    feat: Add group feature to Radio

--- packages/remix/lib/components/radio/radio_widget.dart
+++ packages/remix/lib/components/radio/radio_widget.dart
@@ -1,27 +1,30 @@
 part of 'radio.dart';
 
-class RxRadio extends StatelessWidget {
+class RxRadio<T> extends StatelessWidget {
   const RxRadio({
     super.key,
     required this.value,
     required this.onChanged,
+    required this.groupValue,
     this.variant = RadioVariant.solid,
     this.size = RadioSize.medium,
     this.disabled = false,
     this.style,
   });
 
-  final bool value;
-  final ValueChanged<bool> onChanged;
+  final T value;
+  final ValueChanged<T?> onChanged;
+  final T? groupValue;
+  final bool disabled;
   final RadioVariant variant;
   final RadioSize size;
   final Style? style;
-  final bool disabled;
 
   @override
   Widget build(BuildContext context) {
     return RxBlankRadio(
       value: value,
+      groupValue: groupValue,
       onChanged: onChanged,
       disabled: disabled,
       style: _buildDefaultRadioStyle(style, [size, variant]),
