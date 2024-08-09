f5c7bcb70001cfb4c3e25195ff759f447ee1ec15
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-08-09 15:49:38 -0300

    add test

--- packages/remix/lib/components/button/button.dart
+++ packages/remix/lib/components/button/button.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:mix/mix.dart';
 import 'package:mix_annotations/mix_annotations.dart';
 import 'package:remix/components/spinner/spinner.dart';
+import 'package:remix/helpers/component_builder.dart';
 
 import '../../helpers/variant.dart';
 import '../../tokens/remix_tokens.dart';
