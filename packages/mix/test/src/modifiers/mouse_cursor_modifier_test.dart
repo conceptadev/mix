28364f76b410a040eed36a83d3ddccacaee17bfd
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-08-22 12:16:46 -0300

    feat: Create mouse cursor Decorator (#263)
    * create mouse cursor
    
    * Update doc
    
    * updating to modifier name
    
    * recreating MouseCursosr using code generation
    
    * Update mouse_cursor_modifier.dart

---
+++ packages/mix/test/src/modifiers/mouse_cursor_modifier_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mix/mix.dart';
+
+void main() {
+  testWidgets('uses custom mouseCursor when provided',
+      (WidgetTester tester) async {
+    final cursor = SystemMouseCursors.resizeUpRight;
+    await tester.pumpWidget(
+      Box(
+        style: Style(
+          $box.wrap.cursor(cursor),
+        ),
+      ),
+    );
+
+    final finder = find.byType(MouseRegion);
+    expect(finder, findsOneWidget);
+
+    final mouseRegion = tester.widget<MouseRegion>(finder);
+    expect(mouseRegion.cursor, equals(cursor));
+  });
+}
