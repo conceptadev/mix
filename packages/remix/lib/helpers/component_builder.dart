f608c1d55a98e8cecd2b71bac8d67ba7561b601e
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-08-23 18:03:10 -0300

    exclude files

--- packages/remix/lib/helpers/component_builder.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-typedef XComponentBuilder<T> = Widget Function(T spec);
--- packages/remix/lib/helpers/context_ext.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension BuildContextX on BuildContext {
-  bool get isDarkMode => Theme.of(this).brightness == Brightness.dark;
-  bool get isLightMode => Theme.of(this).brightness == Brightness.light;
-}
