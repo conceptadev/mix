849dd553cc7eedc95191b68569cbd33ff5036d80
Robert Felker <felker.robert@gmail.com>
2024-08-27 18:01:21 +0200

    fold themes into single root (#466)

--- packages/mix/lib/src/theme/mix/mix_theme.dart
+++ packages/mix/lib/src/theme/mix/mix_theme.dart
@@ -132,6 +132,13 @@ class MixThemeData {
     );
   }
 
+  /// Combine all [themes] into a single [MixThemeData] root.
+  static MixThemeData combine(Iterable<MixThemeData> themes) {
+    if (themes.isEmpty) return const MixThemeData.empty();
+    return themes.fold(
+        const MixThemeData.empty(), (previous, theme) => previous.merge(theme));
+  }
+
   @override
   operator ==(Object other) {
     if (identical(this, other)) return true;
