0847430cea679a242721b61220656e08b27d8148
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-09-04 14:04:55 -0300

    feat: Add a way to clear all previous modifiers

--- packages/mix/lib/src/attributes/modifiers/widget_modifiers_data_dto.dart
+++ packages/mix/lib/src/attributes/modifiers/widget_modifiers_data_dto.dart
@@ -6,15 +6,23 @@ import '../../core/factory/mix_data.dart';
 import '../../core/modifier.dart';
 import 'widget_modifiers_data.dart';
 
+class _WidgetModifiersDataDtoCleaner extends WidgetModifiersDataDto {
+  const _WidgetModifiersDataDtoCleaner() : super(const []);
+}
+
 class WidgetModifiersDataDto extends Dto<WidgetModifiersData>
     with Diagnosticable {
   final List<WidgetModifierSpecAttribute> value;
 
   const WidgetModifiersDataDto(this.value);
+  factory WidgetModifiersDataDto.cleaner() {
+    return const _WidgetModifiersDataDtoCleaner();
+  }
 
   @override
   WidgetModifiersDataDto merge(WidgetModifiersDataDto? other) {
     if (other == null) return this;
+    if (other is _WidgetModifiersDataDtoCleaner) return other;
     final thisMap = AttributeMap(value);
     final otherMap = AttributeMap(other.value);
     final mergedMap = thisMap.merge(otherMap).values;
