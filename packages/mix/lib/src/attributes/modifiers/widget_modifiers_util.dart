750aaa73dd1c82d48087e8c0f21c2b32a75a1e37
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-09-10 16:56:52 -0300

    fixes

--- packages/mix/lib/src/attributes/modifiers/widget_modifiers_util.dart
+++ packages/mix/lib/src/attributes/modifiers/widget_modifiers_util.dart
@@ -1,10 +1,13 @@
 import '../../core/attribute.dart';
 import '../../core/modifier.dart';
+import '../../modifiers/internal/cleaner_modifier.dart';
 import '../../modifiers/widget_modifiers_util.dart';
 import 'widget_modifiers_data_dto.dart';
 
 final class SpecModifierUtility<T extends Attribute>
     extends ModifierUtility<T, WidgetModifiersDataDto> {
+  late final clearModifiers = CleanerModifierSpecUtility(only);
+
   SpecModifierUtility(super.builder);
 
   @override
--- packages/mix/lib/src/modifiers/widget_modifiers_util.dart
+++ packages/mix/lib/src/modifiers/widget_modifiers_util.dart
@@ -6,7 +6,6 @@ import 'aspect_ratio_widget_modifier.dart';
 import 'clip_widget_modifier.dart';
 import 'flexible_widget_modifier.dart';
 import 'fractionally_sized_box_widget_modifier.dart';
-import 'internal/cleaner_modifier.dart';
 import 'intrinsic_widget_modifier.dart';
 import 'mouse_cursor_modifier.dart';
 import 'opacity_widget_modifier.dart';
@@ -31,7 +30,6 @@ abstract class ModifierUtility<T extends Attribute, Value>
   late final visibility = VisibilityModifierSpecUtility(only);
   late final show = visibility.on;
   late final hide = visibility.off;
-  late final clearModifiers = CleanerModifierSpecUtility(only);
   late final aspectRatio = AspectRatioModifierSpecUtility(only);
   late final flexible = FlexibleModifierSpecUtility(only);
   late final expanded = flexible.expanded;
