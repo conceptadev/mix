d3b813e2681f6ff0a9f3c506fc9d81513eadcb8b
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-09-27 17:03:52 -0300

    lint

--- packages/mix/lib/src/attributes/border/border_dto.dart
+++ packages/mix/lib/src/attributes/border/border_dto.dart
@@ -1,10 +1,8 @@
 // ignore_for_file: prefer_relative_imports,avoid-importing-entrypoint-exports
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mix/mix.dart';
 import 'package:mix_annotations/mix_annotations.dart';
 
-import '../../core/factory/mix_provider.dart';
 import '../../internal/mix_error.dart';
 
 part 'border_dto.g.dart';
