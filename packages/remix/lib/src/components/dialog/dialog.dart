aaf6f89d320c6f164659bdc4b8caa218c9b2a2ab
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-10-08 14:08:48 -0300

    create dialog component

---
+++ packages/remix/lib/src/components/dialog/dialog.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:mix/mix.dart';
+import 'package:mix_annotations/mix_annotations.dart';
+
+import '../../helpers/component_builder.dart';
+import '../../theme/remix_theme.dart';
+import '../../theme/remix_tokens.dart';
+
+part 'dialog.g.dart';
+part 'dialog_style.dart';
+part 'dialog_theme.dart';
+part 'dialog_widget.dart';
+
+Future<T?> showDialog<T>({
+  required BuildContext context,
+  required WidgetBuilder builder,
+  bool barrierDismissible = true,
+  Color barrierColor = const Color(0xcc000000),
+  String barrierLabel = '',
+  bool useRootNavigator = true,
+  RouteSettings? routeSettings,
+  Offset? anchorPoint,
+  Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+      transitionBuilder,
+  Duration transitionDuration = const Duration(milliseconds: 200),
+}) {
+  transitionBuilder ??= (context, animation, secondaryAnimation, child) {
+    final curvedAnimation = CurvedAnimation(
+      parent: animation,
+      curve: Curves.easeInOut,
+    );
+    return FadeTransition(
+      opacity: curvedAnimation,
+      child: ScaleTransition(
+        scale: curvedAnimation.drive(Tween(begin: 0.85, end: 1)),
+        child: child,
+      ),
+    );
+  };
+
+  return showGeneralDialog(
+    context: context,
+    pageBuilder: (context, animation, secondaryAnimation) => builder(context),
+    barrierColor: barrierColor,
+    barrierDismissible: barrierDismissible,
+    barrierLabel: barrierLabel,
+    useRootNavigator: useRootNavigator,
+    routeSettings: routeSettings,
+    anchorPoint: anchorPoint,
+    transitionBuilder: transitionBuilder,
+    transitionDuration: transitionDuration,
+  );
+}
+
+@MixableSpec()
+base class DialogSpec extends Spec<DialogSpec>
+    with _$DialogSpec, Diagnosticable {
+  final BoxSpec container;
+  final TextSpec title;
+  final TextSpec description;
+  final FlexSpec mainFlex;
+  final FlexSpec actionsFlex;
+
+  /// {@macro card_spec_of}
+  static const of = _$DialogSpec.of;
+
+  static const from = _$DialogSpec.from;
+
+  const DialogSpec({
+    BoxSpec? container,
+    TextSpec? title,
+    TextSpec? description,
+    FlexSpec? mainFlex,
+    FlexSpec? actionsFlex,
+    super.modifiers,
+    super.animated,
+  })  : container = container ?? const BoxSpec(),
+        title = title ?? const TextSpec(),
+        description = description ?? const TextSpec(),
+        mainFlex = mainFlex ?? const FlexSpec(),
+        actionsFlex = actionsFlex ?? const FlexSpec();
+
+  // Widget call({Key? key, required List<Widget> children}) {
+  //   return DialogSpecWidget(key: key, spec: this, children: children);
+  // }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    _debugFillProperties(properties);
+  }
+}
