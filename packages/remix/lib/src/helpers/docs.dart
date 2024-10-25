82d4adf67efa7683238b179632fe96b0e0df3501
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2024-10-25 16:29:38 -0300

    avatar

---
+++ packages/remix/lib/src/helpers/docs.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: unused_element
+
+library docs;
+
+/// {@template remix.widget_spec_builder.text_spec}
+/// The builder offers a [TextSpec] which can be used to style the
+/// text consistently with component's style.
+/// {@endtemplate}
+/// {@template remix.widget_spec_builder.text_spec.code}
+/// ```dart
+///
+/// ```
+/// {@endtemplate}
+const _widgetSpecBuilder = '';
+
+/// {@template use_style_from_context}
+/// If not provided, the style will be get from the context.
+/// {@endtemplate}
+const _useStyleFromContext = '';
