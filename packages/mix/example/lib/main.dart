ff69fd84e0d47061361bd908b99fdedb659d1ee7
Hesham Al Nahas <121490059+hesham04Dev@users.noreply.github.com>
2024-10-31 15:56:56 +0200

    adding example in mix package the example increase the package score

---
+++ packages/mix/example/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:mix/mix.dart';
+
+void main() async {
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+final style = Style(
+  $box.height(100),
+  $box.width(100),
+  $box.margin(10, 20),
+  $box.color.blue(),
+  $box.borderRadius(10),
+  $box.padding(20, 10),
+  $box.margin(10),
+  $box.border(
+    color: Colors.black,
+    width: 1,
+    style: BorderStyle.solid,
+  ),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Box(
+        style: style,
+        child: const Center(child: Text('Hello Mix')),
+      ),
+    ));
+  }
+}
